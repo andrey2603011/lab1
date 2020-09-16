54df8809a12a20af6b1ddfab637f71a397322d4e
andrey2603011 <andrey2603011@gmail.com>
2020-09-16 20:11:01 +0300

    Initial commit

---
+++ src/lab1/Testball.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.lang.*;
+public class Testball {
+    public static void main(String[] args) {
+        Ball ba1 = new Ball("red", 70, 410);
+        Ball ba2 = new Ball("blue", 75, 420);
+        Ball ba3 = new Ball("pink", 80, 450);
+        System.out.println(ba1);
+        System.out.println(ba2);
+        System.out.println(ba3);
+    }
+}
---
+++ src/lab1/Testbook.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.lang.*;
+public class Testbook {
+    public static void main(String[] args){
+        Book b1 = new Book("The count of Monte Cristo",1300);
+        Book b2 = new Book("Taras Bulba",115);
+        Book b3 = new Book("dead Souls",470);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+    }
+}
